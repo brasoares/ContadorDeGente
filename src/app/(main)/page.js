6c03f4f11a94ce3483fefb59e42ce89b7811bc9a
Alan Gleizer <alan@mgleizer.com>
2023-10-22 12:29:01 -0300

    detalhes

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,6 +1,4 @@
-import PorLocal from "@/app/(main)/porlocal/PorLocal";
 import VisaoGeral from "@/app/(main)/visaogeral/VisaoGeral";
-import Example from "@/app/(main)/calendario/page";
 
 export default function Home() {
   return (
