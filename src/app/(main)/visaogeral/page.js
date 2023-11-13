931da7256e85bb90f16eaf735a73f14a4e8a2a0f
Henoc Soares Freire <henocsoares@hotmail.com.br>
2023-11-13 15:52:06 -0300

    Delete src/app/(main) directory
    Working one will be added

--- src/app/(main)/visaogeral/page.js
+++
@@ -1,8 +0,0 @@
-import VisaoGeral from "@/app/(main)/visaogeral/VisaoGeral";
-
-export default function Home() {
-  return (
-    <>
-      <VisaoGeral />
-    </>);
-}
--- src/app/(main)/visaogeral/visaoGeral-components/estatisticaGeral.jsx
+++
@@ -1,8 +0,0 @@
-export default function EstatisticaGeral(props) {
-    return (
-    <div className='grid justify-items-center'>
-        <p className="font-extrabold text-black">{props.numero}</p>
-        <p className='font-bold text-sm' class="estatistica-texto">{props.nome}</p>
-    </div>
-    )
-    }
